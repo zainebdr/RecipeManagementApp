6293f711018bb2634c1eadc2851c4672caa9cc38
zainebdr <zaineb.drira@outlook.com>
2023-10-11 16:30:25 +0200

    Error handling set

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -1,5 +1,6 @@
+const createError = require('http-errors');
 const Recipe = require('../models/RecipeModel');// Import Recipe model
-
+const mongoose = require('mongoose');
 //Get All Recipes
 exports.getAllRecipes = async (req, res, next) => {
     try { 
@@ -12,18 +13,24 @@ exports.getAllRecipes = async (req, res, next) => {
 
 
   //Get a specific Recipe by id
-exports.getRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id);
-    if(!recipe) {
-      return res.status(404).json({ message: 'Recipe not found' });
+  exports.getRecipe = async (req, res, next) => {
+    try {
+      const recipe = await Recipe.findById(req.params.id);
+      if (!recipe) {
+        // Use http-errors to create a 404 error and throw it
+        throw createError(404, 'Recipe not found');
+      }
+  
+      res.json(recipe);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        next(createError(400, 'Invalid Recipe Id'));
+        return;
+      }
+      next(error);
     }
-    res.json(recipe);
-  } catch (err) {
-    next(err); // Pass the error to the error handler
-  }
-   
-};
+  };
 
   //Create a new Recipe
   exports.createRecipe = async (req, res, next) => {
@@ -44,13 +51,20 @@ exports.getRecipe = async (req, res, next) => {
     try{
         updatedRecipe = req.body;
         let recipe=await Recipe.findByIdAndUpdate(req.params.id,updatedRecipe);//function that performs both finding a document by its ID and updated it in a single operation. 
-             console.log("recipe", recipe  ) //recipe is assigned the value of the removed recipe.
-        if(!recipe){return res.status(401).send('Recipe not found')}
-        return res.status(200).json(recipe);
-
-      }catch(err){
-        return next(err);
+         //    console.log("recipe", recipe  ) //recipe is assigned the value of the removed recipe.
+         if (!recipe) {
+          // Use http-errors to create a 404 error and throw it
+          throw createError(404, 'Recipe not found');
         }
+        res.json(recipe);
+      } catch (error) {
+        console.log(error.message);
+        if (error instanceof mongoose.CastError) {
+          next(createError(400, 'Invalid Recipe Id'));
+          return;
+        }
+        next(error);
+      }
             
   }
 
@@ -63,10 +77,18 @@ exports.getRecipe = async (req, res, next) => {
        
         let recipe=await Recipe.findByIdAndRemove(recipeId);//function that performs both finding a document by its ID and removing it in a single operation. 
              console.log("recipe", recipe  ) //recipe is assigned the value of the removed recipe.
-        if(!recipe){return res.status(401).send('Recipe not found')}
-        return res.status(200).json(recipe);
-
-      }catch(err){
-        return next(err);
-        }     
+             if (!recipe) {
+              // Use http-errors to create a 404 error and throw it
+              throw createError(404, 'Recipe not found');
+            }
+        
+            res.json(recipe);
+          } catch (error) {
+            console.log(error.message);
+            if (error instanceof mongoose.CastError) {
+              next(createError(400, 'Invalid Recipe Id'));
+              return;
+            }
+            next(error);
+          }
   }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const createError = require('http-errors');
 const mongoose = require('mongoose');
 const recipesRouter=require('./routes/RecipesRoutes')
 
@@ -8,16 +8,28 @@ const app = express()
 
 //add middleware
 app.use(express.json())
+
+
+//connection to mongo db
+mongoose.connect('mongodb://127.0.0.1:27017/RecipesMangDb', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('Mongodb connected....')});
+const db=mongoose.connection
+db.on('error',(err)=> console.error(err));
+db.once('open',()=>{
+    console.log("DB open connected")
+});
+
+
 app.use('/api/recipes', recipesRouter);
 
-
+//404 handler and pass to error handler
 app.use((req,res,next)=>{
-    //res.status(404)
-    //res.send("erooor not found")
-    const err =new Error("Not found");
+   /* const err =new Error("Not found");
     err.status=404;
     //when we call next and we pass err then the error handler will be called    
-    next(err);
+    next(err);*/
+    next(createError(404, 'Not found'));
 });
 
 // Error handler middleware
@@ -35,15 +47,4 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 3000
 // ` so we can use a template string  
 app.listen(port,()=>{console.log(` Listining on port ${port}..` )}  )
-
-
-//connection to mongo db
-mongoose.connect('mongodb://127.0.0.1:27017/RecipesMangDb', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-    console.log('Mongodb connected....')});
-const db=mongoose.connection
-db.on('error',(err)=> console.error(err));
-db.once('open',()=>{
-    console.log("DB open connected")
-});
 
