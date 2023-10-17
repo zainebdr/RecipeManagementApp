b3ee9ef1d94c2e8e1f6fe2ee011845ccf17a95aa
zainebdr <zaineb.drira@outlook.com>
2023-10-17 13:55:00 +0200

    final commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-const User=require('../models/UserModel')
+const User= require('../models/UserModel')
 const jwt = require('jsonwebtoken');
 // controller actions
 module.exports.signup_get = (req, res) => {
@@ -26,13 +26,12 @@ const createToken = (id) => {
  // Route for checking if an email exists
   module.exports.getEmail= async (req, res) => {
   try {
-    const { email } = req.query;
-    console.log('reeeees');
-    const existingUser = User.find({ email: email });
-    
+    const email = req.params['email'];
+    const existingUser = await User.findOne({ email: email });
     if (existingUser) {
-      res.json({ exists: true });
-      console.log('exist ',existingUser);
+     
+      const token = createToken(existingUser._id);
+      res.json({ exists: true , jwt:token });
         } else {
       res.json({ exists: false });
       console.log('not exist',existingUser);
@@ -47,15 +46,9 @@ const createToken = (id) => {
   module.exports.signup_post = async (req, res, next) => {
     try {
       const { email, password } = req.body;
-    /*  const userWithEmail = await User.find((user) => user.email === email);
-      console.log('emaail ',userWithEmail);
-        if (userWithEmail) {
-          // L'e-mail existe déjà, renvoyer une réponse appropriée (par exemple, un code d'état 409 Conflict)
-          return res.status(409).json({ message: 'L\'e-mail existe déjà' });
-        }*/
       const newuser = new User({ email, password });
       const savedUser = await newuser.save();
-     
+
       //create token
       const token = createToken(savedUser._id);
       console.log('token ', token);
--- routes/RecipesRoutes.js
+++ routes/RecipesRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const recipeController =require('../controllers/RecipeController');
-
+const auth=require('../middleware/Auth');
 //Retrieve a list of all recipes.
-router.get('/',recipeController.getAllRecipes);
+router.get('/',auth,recipeController.getAllRecipes);
 
 //Retrieve a specific recipe by ID.
-router.get('/:id',recipeController.getRecipe);
+router.get('/:id',auth,recipeController.getRecipe);
 
 // Create a new recipe.
-router.post('/',recipeController.createRecipe);
+router.post('/',auth,recipeController.createRecipe);
 
 //Update a specific recipe by ID.
-router.put('/:id',recipeController.updateRecipe);
+router.put('/:id',auth,recipeController.updateRecipe);
 
 //Delete a specific recipe by ID.
-router.delete('/:id',recipeController.destroyRecipe);
+router.delete('/:id',auth,recipeController.destroyRecipe);
 
 
 // Export the router to be used in app.js.
